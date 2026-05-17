571e8f4b0b8e278407c487a1efb10830242e8889
Rageus17 <stevenlv17@gmail.com>
2026-05-17 16:56:21 +0700

    monthly trx pnl

---
+++ models/report/rpt_toko__monthly_trx_pnl.sql
@@ -0,0 +1,25 @@
+with 
+    pnl_trx as (select * from {{ref('mart_toko__trx_pnl_details')}}),
+    transaksi as (select * from {{ref('stg_toko__transaksi')}}),
+
+    diskon_pnl as (
+        select 
+            p.*,
+            coalesce(t.diskon_nominal,0) as diskon_nominal
+        from    
+            pnl_trx p
+        left join 
+            transaksi t
+        on 
+            p.transaksi_id = t.transaksi_id
+    )
+
+    select 
+        date_trunc('month',created_at) as month_id,
+        sum(nett_total) as total_transaksi,
+        sum(diskon_nominal) as diskon_transaksi,
+        sum(pnl) as pnl
+    from 
+        diskon_pnl
+    group by 1
+    order by 1 desc
