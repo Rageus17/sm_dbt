04aefac8d786e6740f54d14338a789988b3283cc
Rageus17 <stevenlv17@gmail.com>
2026-05-31 11:24:41 +0700

    daily PNL report

--- models/report/rpt_toko__daily_trx_pnl.sql
+++ models/report/rpt_toko__daily_trx_pnl.sql
@@ -15,7 +15,7 @@ with
     )
 
     select 
-        date_trunc('day',created_at) as month_id,
+        toDate(created_at, 'Asia/Jakarta') as day_id,
         sum(nett_total) as total_transaksi,
         sum(diskon_nominal) as diskon_transaksi,
         sum(pnl) as pnl
